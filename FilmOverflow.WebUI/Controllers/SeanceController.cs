763dddd9ea63f5f4fe661fd78afd5c974d3aad46
Ivan Fantom <ziv78@yandex.ru>
2014-08-20 11:03:17 +0300

    adding seance management

--- FilmOverflow.WebUI/Controllers/SeanceController.cs
+++ FilmOverflow.WebUI/Controllers/SeanceController.cs
@@ -10,90 +10,90 @@ using FilmOverflow.WebUI.ViewModels;
 
 namespace FilmOverflow.WebUI.Controllers
 {
-    public class SeanceController : Controller
-    {
+	public class SeanceController : Controller
+	{
 		private readonly ISeanceService _seanceService;
 
-	    public SeanceController(ISeanceService seanceService)
-	    {
+		public SeanceController(ISeanceService seanceService)
+		{
 			if (seanceService == null)
 			{
 				throw new ArgumentNullException("seanceService");
 			}
 			_seanceService = seanceService;
-	    }
+		}
 
-        public ActionResult Index()
-        {
+		public ActionResult Index()
+		{
 			var seancesDomainModel = _seanceService.Read();
 			var seancesViewModel = Mapper.Map<IEnumerable<SeanceDomainModel>, IEnumerable<SeanceViewModel>>(seancesDomainModel);
 
 			return PartialView("_IndexPartial", seancesViewModel);
-        }
+		}
 
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+		public ActionResult Create()
+		{
+			return View();
+		}
 
-        public ActionResult Create()
-        {
-            return View();
-        }
+		[HttpPost]
+		public ActionResult Create(FormCollection collection)
+		{
+			try
+			{
+				// TODO: Add insert logic here
 
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
 
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+		[HttpPost]
+		public ActionResult Edit(int id, FormCollection collection)
+		{
+			try
+			{
+				// TODO: Add update logic here
 
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+		
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
+		
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+		[HttpPost]
+		public ActionResult Delete(int id, FormCollection collection)
+		{
+			try
+			{
+				// TODO: Add delete logic here
 
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-    }
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
 }
