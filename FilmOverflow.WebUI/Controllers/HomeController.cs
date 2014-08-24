7cd8a3adf17103367c38da0440f23b5199d4734d
Ivan Zakhartchouk <ivannn787@gmail.com>
2014-08-24 23:47:10 +0300

    added Film list on the Home page, Film details page

--- FilmOverflow.WebUI/Controllers/HomeController.cs
+++ FilmOverflow.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Web.Mvc;
 using AutoMapper;
 using FilmOverflow.Domain.Models;
@@ -10,15 +12,50 @@ namespace FilmOverflow.WebUI.Controllers
 	public class HomeController : Controller
 	{
 		private readonly IUserManagerService _userManagerService;
-		public HomeController(IUserManagerService userManagerService)
+		private readonly IFilmService _filmService;
+
+		public HomeController(IUserManagerService userManagerService, IFilmService filmService)
 		{
 			_userManagerService = userManagerService;
+			_filmService = filmService;
 		}
 
 		public ActionResult Index()
 		{
 			return View();
 		}
+
+		public ActionResult List()
+		{
+			IEnumerable<FilmDomainModel> filmsDomainModel = _filmService.Read();
+			IEnumerable<FilmViewModel> filmsViewModel = Mapper.Map<IEnumerable<FilmDomainModel>, IEnumerable<FilmViewModel>>(filmsDomainModel);
+
+			return PartialView("_ListPartial", filmsViewModel);
+		}
+
+		public ActionResult Details(long? filmId)
+		{
+			if (filmId == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+
+			FilmDomainModel filmDomainModel = _filmService.ReadById(filmId);
+			FilmViewModel filmViewModel = Mapper.Map<FilmDomainModel, FilmViewModel>(filmDomainModel);
+
+			if (filmViewModel == null)
+			{
+				return HttpNotFound();
+			}
+
+			return View("Details", filmViewModel);
+		}
+
+		public ActionResult Browse()
+		{
+			return View();
+		}
+
 		public ActionResult UserInfo()
 		{
 			var currentUser = _userManagerService.FindByName(User.Identity.Name);
